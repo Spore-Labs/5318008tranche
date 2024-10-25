829eed33d8a105d602234fd36ea6abffa62f7850
Mycelium <SporeNFTs@proton.me>
2024-10-25 18:00:39 +1000

    frame-ancestors for walletconnect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-   /** @type {import('next').NextConfig} */
-   const nextConfig = {
-    reactStrictMode: true,
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://secure.walletconnect.org/",
+          },
+        ],
+      },
+    ]
+  },
+}
 
-  module.exports = nextConfig
+module.exports = nextConfig
