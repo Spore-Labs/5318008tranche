c668f69174ecbde59187c8c137caf5b19667bb30
Mycelium <SporeNFTs@proton.me>
2024-11-09 16:21:10 +1000

    fix 4hr chart

--- app/components/CandlestickChart.tsx
+++ app/components/CandlestickChart.tsx
@@ -183,12 +183,12 @@ const CandlestickChart: React.FC<CandlestickChartProps> = ({ data, timeFrame, me
       case '1h':
       case '4h':
         if (isXsScreen) {
-          return utcHours % 6 === 0 && utcMinutes === 0; // Every 6 hours
+          return utcHours % 12 === 0 && utcMinutes === 0; // Every 12 hours
         }
         if (isMobileOrSmall) {
-          return utcHours % 4 === 0 && utcMinutes === 0; // Every 4 hours
+          return utcHours % 8 === 0 && utcMinutes === 0; // Every 8 hours
         }
-        return utcHours % 2 === 0 && utcMinutes === 0; // Every 2 hours
+        return utcHours % 4 === 0 && utcMinutes === 0; // Every 4 hours
       case '1d':
         if (isXsScreen) {
           return utcDay % 3 === 0; // Every 3 days
